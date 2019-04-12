d04d4421862ffb007208f9d84fad963ccb95f81b
Joseph <z@charmed.tech>
2019-04-12 09:33:00 -0700

    Add Q error event publishers to error handlers

--- src/middleware/404.js
+++ src/middleware/404.js
@@ -5,8 +5,13 @@
  * @module middleware/404
  */
 
+// Instantiate Q client
+const Q = require('@nmq/q/client');
+
 /**
  * Sends a custom 404 response
+ * Publishes the url as an `error` event to the
+ * `database` namespace of the message queue
  * @function
  * @param req {object} Express request object
  * @param res {object} Express response object
@@ -14,6 +19,10 @@
  */
 module.exports = (req, res, next) => {
   const error = { error: 'Resource Not Found' };
+
+  const { url } = req;
+  Q.publish('database', 'error', { url });
+
   res.statusCode = 404;
   res.statusMessage = 'Not Found';
   res.setHeader('Content-Type', 'application/json');
--- src/middleware/500.js
+++ src/middleware/500.js
@@ -5,8 +5,13 @@
  * @module middleware/error
  **/
 
+// Instantiate Q client
+const Q = require('@nmq/q/client');
+
 /**
  * Error 500 handler - Returns a JSON object on a server error
+ * Publishes the url and error message as an `error` event to the
+ * `database` namespace of the message queue
  * @function
  * @param err {object} Express error object
  * @param req {object} Express request object
@@ -16,6 +21,10 @@
 module.exports = (err, req, res, next) => {
   console.error('__SERVER_ERROR__', err);
   let error = { error: err.message || err };
+
+  const { url } = req;
+  Q.publish('database', 'error', { url, error });
+
   res.statusCode = err.status || 500;
   res.statusMessage = err.statusMessage || 'Server Error';
   res.setHeader('Content-Type', 'application/json');
